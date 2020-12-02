ad7b16689150b6af361c995687ec5fd147b671c3
Katelinn Jones <katiejonesyo@gmail.com>
2020-12-01 16:45:20 -0800

    test passing using spreaddddd

--- copyAndPush.test.js
+++ copyAndPush.test.js
@@ -1,11 +1,14 @@
 const { copyAndPush } = require('./copyAndPush.js');
 
 describe('copyAndPush', () => {
+    const numbers = [1, 2, 3];
+    
     it('add item to array and return array', () => {
-        const numbers = [1, 2, 3];
         const result = copyAndPush(numbers, 4);
-
         expect(result).toEqual([1, 2, 3, 4])
+    })
 
+    it('makes sure that the original array is unchanged', () => {
+        expect(numbers).toEqual([1, 2, 3])
     })
 });
